d5c14dc87a9c755c81e2334131143f6b87db5c7b
bakebrlk <210103194@stu.sdu.edu.kz>
2024-02-18 03:57:17 +0600

    updateWelcomePage

--- ToDoList/ToDoList/model/WelcomePageModel.swift
+++ ToDoList/ToDoList/model/WelcomePageModel.swift
@@ -20,16 +20,68 @@ struct WelcomePageModel: View{
     
     @State private var imageView = Image("")
     
+    private var nextPage: some View {
+        
+        
+            Button(
+                action: {
+                    id += 1
+                    reloadData()
+                },
+                label: {
+                    
+                    HStack{
+
+                        Spacer()
+                        textView(text: "Let's Start", font: .system(.title3, weight: .bold))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity)
+                            .padding(.leading)
+                                                
+                        Spacer()
+
+                        textView(text: "->", font: .system(.title3, weight: .bold))
+                            .foregroundColor(.white)
+                            .padding(.trailing)
+                    }
+            })
+        
+        
+        
+    }
+    
     var body: some View {
-        VStack {
-            imageView
-                .padding()
-            titleView
-                .padding()
-            descriptionView
-        }
-        .onAppear{
-            reloadData()
+        
+        GeometryReader { make in
+            
+            VStack {
+                Spacer()
+                
+                imageView
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+                
+                Spacer()
+                
+                titleView
+                    .multilineTextAlignment(.center)
+                    .padding()
+                
+                descriptionView
+                    .multilineTextAlignment(.center)
+                    .padding()
+                Spacer()
+                
+                nextPage
+                    .frame(maxWidth: .infinity, maxHeight: make.size.height/10)
+                    .background(Color.purple)
+                    .cornerRadius(18)
+                    .padding()
+            }
+            .onAppear{
+                reloadData()
+            }
         }
     }
 }
@@ -40,8 +92,8 @@ extension WelcomePageModel {
         desciption = Data.WelcomePage.data[id].description
         imageName = Data.WelcomePage.data[id].imageName
         
-        titleView = textView(text: title, font: .title)
-        descriptionView = textView(text: desciption, font: .largeTitle)
+        titleView = textView(text: title, font: .system(size: 36, weight: .medium))
+        descriptionView = textView(text: desciption, font: .system(size: 22, weight: .thin))
         imageView = Image(imageName)
             
     }
@@ -51,6 +103,7 @@ extension WelcomePageModel {
     func textView(text: String, font: Font) -> Text{
         Text(text)
             .font(font)
+        
     }
 }
 
