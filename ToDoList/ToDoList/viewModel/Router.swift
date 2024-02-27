d9194a57b2b5eb4eae330d7f0774dcda07344080
bakebrlk <210103194@stu.sdu.edu.kz>
2024-02-27 23:40:51 +0600

    addRouter

---
+++ ToDoList/ToDoList/viewModel/Router.swift
@@ -0,0 +1,31 @@
+//
+//  Router.swift
+//  ToDoList
+//
+//  Created by bakebrlk on 27.02.2024.
+//
+
+import SwiftUI
+
+enum Router{
+    
+    case welcome
+    case checkAccount
+    case singIn
+    case signUp
+    case updatePassword
+    case sendMail
+    
+    @ViewBuilder
+    var builder: some View {
+        switch(self) {
+        case .welcome: WelcomePageView()
+        case .checkAccount: CheckAccountView()
+        case .singIn: SignInView()
+        case .signUp: SignUpView()
+        case .updatePassword: UpdatePasswordView()
+        case .sendMail: SendMailView()
+            
+        }
+    }
+}
