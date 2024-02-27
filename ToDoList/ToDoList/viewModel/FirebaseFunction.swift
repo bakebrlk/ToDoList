8ec334587547a0fc6a1ae4977601af17e00d7390
bakebrlk <210103194@stu.sdu.edu.kz>
2024-02-27 13:35:08 +0600

    addFirebaseSignUp

--- ToDoList/ToDoList/viewModel/FirebaseFunction.swift
+++ ToDoList/ToDoList/viewModel/FirebaseFunction.swift
@@ -7,11 +7,17 @@
 
 import Foundation
 import SwiftUI
+import FirebaseAuth
 
 struct FirebaseFunction{
     
-    static func signUp() -> Bool{
-        return true
+    static let shared = FirebaseFunction()
+    
+    private init() {}
+    
+    static func signUp(email: String, password: String) async throws -> AuthDataResultModel{
+        let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
+        return AuthDataResultModel(user: authDataResult.user)
     }
     
     static func signIn() -> Bool{
