fe6dd9c923d899e73442b18817d73c6d931be2d3
bakebrlk <210103194@stu.sdu.edu.kz>
2024-02-27 13:35:57 +0600

    addAuthenticationStruct

---
+++ ToDoList/ToDoList/viewModel/Authentication.swift
@@ -0,0 +1,47 @@
+//
+//  Auth.swift
+//  ToDoList
+//
+//  Created by bakebrlk on 27.02.2024.
+//
+
+import FirebaseAuth
+import SwiftUI
+
+struct Authentication{
+    
+    static func signUp(email: String, password: String, confPassword: String) -> Bool{
+        
+        guard !email.isEmpty && !password.isEmpty && confPassword == password && validPassword(password: password) else{
+            return false
+        }
+        
+        Task {
+            do {
+                let returnedUserData = try await FirebaseFunction.signUp(email: email, password: password)
+                print(returnedUserData)
+                return true
+            }catch {
+                print(error)
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    private static func validPassword(password: String) -> Bool {
+        
+        let uppercaseLetterRegex = ".*[A-Z]+.*"
+        let lowercaseLetterRegex = ".*[a-z]+.*"
+        let numberRegex = ".*[0-9]+.*"
+
+        let uppercaseTest = NSPredicate(format:"SELF MATCHES %@", uppercaseLetterRegex)
+        let lowercaseTest = NSPredicate(format:"SELF MATCHES %@", lowercaseLetterRegex)
+        let numberTest = NSPredicate(format:"SELF MATCHES %@", numberRegex)
+
+        return uppercaseTest.evaluate(with: password) &&
+                      lowercaseTest.evaluate(with: password) &&
+                      numberTest.evaluate(with: password)
+    }
+}
