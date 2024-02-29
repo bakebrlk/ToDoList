2927384e9a40d3f534f89c43ee063a169feb74ca
bakebrlk <210103194@stu.sdu.edu.kz>
2024-03-01 01:09:34 +0500

    addCustomImage

--- ToDoList/ToDoList/model/CustomImage.swift
+++ ToDoList/ToDoList/model/CustomImage.swift
@@ -5,4 +5,28 @@
 //  Created by bakebrlk on 01.03.2024.
 //
 
-import Foundation
+import SwiftUI
+
+final class CustomImage{
+    static func getImage(imageName: String) -> some View {
+        Image(imageName)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+    }
+    
+    static func getImage(systemName: String) -> some View {
+        Image(systemName: systemName)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+    }
+    
+    static func getImage(uiImage: UIImage, height: CGFloat) -> some View {
+        VStack{
+            Image(uiImage: uiImage)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+        }
+        .frame(maxWidth: height, maxHeight: height)
+        .cornerRadius(height/2)
+    }
+}
