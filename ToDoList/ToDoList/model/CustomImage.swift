78d3e51a174886dab9e9d3f68472461112b0de09
bakebrlk <210103194@stu.sdu.edu.kz>
2024-03-01 01:09:22 +0500

    addImagePicker

---
+++ ToDoList/ToDoList/model/CustomImage.swift
@@ -0,0 +1,8 @@
+//
+//  CustomImage.swift
+//  ToDoList
+//
+//  Created by bakebrlk on 01.03.2024.
+//
+
+import Foundation
---
+++ ToDoList/ToDoList/view/HomePageView.swift
@@ -0,0 +1,18 @@
+//
+//  HomePageView.swift
+//  ToDoList
+//
+//  Created by bakebrlk on 01.03.2024.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePageView()
+}
