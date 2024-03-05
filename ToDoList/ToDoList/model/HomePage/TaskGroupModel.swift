448bd27ec100c3d684aefa7fa6514506e7efbcf5
bakebrlk <210103194@stu.sdu.edu.kz>
2024-03-06 00:44:43 +0500

    testSize

---
+++ ToDoList/ToDoList/model/HomePage/TaskGroupModel.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  ToDoList
+//
+//  Created by bakebrlk on 06.03.2024.
+//
+
+import Foundation
--- ToDoList/ToDoList/viewModel/Size.swift
+++ ToDoList/ToDoList/viewModel/Size.swift
@@ -8,5 +8,6 @@
 import SwiftUI
 
 struct Size{
-    static var size = getSize()
+//    static var size = (sizeStatus()) ? [400,400]: getSize()
+    static var size = [400.0,600.0]
 }
