081c1ddf3d1f6c667f97776f1ac34ceb0188ff81
bakebrlk <210103194@stu.sdu.edu.kz>
2024-03-06 02:26:17 +0500

    addCalendarPage

---
+++ ToDoList/ToDoList/view/CalendarPageView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarView.swift
+//  ToDoList
+//
+//  Created by bakebrlk on 06.03.2024.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarView()
+}
