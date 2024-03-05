60e2bd105a92c0c3c939fa518050451ce0438b30
bakebrlk <210103194@stu.sdu.edu.kz>
2024-03-06 01:57:25 +0500

    addNewFunctionTaskGroup

--- ToDoList/ToDoList/model/HomePage/cartModel.swift
+++ ToDoList/ToDoList/model/HomePage/cartModel.swift
@@ -15,9 +15,10 @@ struct cartModel{
                 textView(text: model.group, size: 14)
                     .foregroundStyle(Color(UIColor.systemGray6))
                 Image(model.logo)
-                    .frame(width: 25, height: 25)
-                    .cornerRadius(8)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
             }
+            .frame(maxHeight: Size.size[1]*0.05)
             .padding()
             
             textView(text: model.title, size: 18)
@@ -28,6 +29,30 @@ struct cartModel{
         }
         .frame(width: Size.size[0]*0.55)
         .background(model.color.opacity(0.5))
+        .cornerRadius(20)
+        .padding(.leading)
+    }
+    
+    static func getTaskGroup(model: TaskGroupModel) -> some View {
+        HStack{
+            Image(model.ava)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding()
+            
+            VStack(alignment: .leading){
+                textView(text: model.title, size: 14)
+                textView(text: "\(model.count) Tasks", size: 10)
+                    .foregroundColor(Color(.systemGray))
+                    .padding(.top,0.1)
+            }
+            Spacer()
+            
+            CircleModel(process: .constant(model.process), textSize: 14, textColor: .black, colors: [model.color]).statisticsCircles(width: 50, height: 50)
+                .padding(.trailing)
+        }
+        .frame(width: Size.size[0]*0.9, height: Size.size[1]*0.12)
+        .background(.white.opacity(0.6))
         .cornerRadius(20)
         .padding(.leading)
     }
