b31641c1384d6bda52e368cf3ffbb1b0414f3a47
bakebrlk <210103194@stu.sdu.edu.kz>
2024-03-06 00:45:22 +0500

    TaskGroupModel

--- ToDoList/ToDoList/model/HomePage/TaskGroupModel.swift
+++ ToDoList/ToDoList/model/HomePage/TaskGroupModel.swift
@@ -5,4 +5,12 @@
 //  Created by bakebrlk on 06.03.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct TaskGroupModel:Identifiable, Hashable{
+    var id: ObjectIdentifier
+    let title: String
+    let count: Int
+    let ava: String
+    let process: CGFloat
+}
